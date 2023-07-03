790a6c0955180bdf248eb663b36b9a7e70cac250
SherondaMD <sheronda.mckee@gmail.com>
2023-07-02 20:46:21 -0700

    Current Temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("description");
@@ -7,15 +7,17 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
 
-  temperatureElement.innnerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  precipitationElement.innerHTML = response.data.main.precipitation;
-  humidityElement.innerHTML = response.data.main.humidity;
+  temperatureElement.innnerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  precipitationElement.innerHTML = response.data.temperature.precipitation;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
-
 let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?q=Phoenix&appid=${apiKey}&units=imperial`;
+let city = "Phoenix";
 
-axios.get(apiUrl).then(displayTemperature);
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Phoenix&key=b8b61a4d34f03tcbf3192f94o59a0ba4&units=imperial";
+
+axios.get(`${apiUrl}`).then(displayTemperature);
