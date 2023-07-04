9167363e7a789ccac41a3a17a6ac6c8598d43bf2
SherondaMD <sheronda.mckee@gmail.com>
2023-07-04 11:41:52 -0700

    placed HTML in JS, created forecast rows, prepped for API integration

--- src/app.js
+++ src/app.js
@@ -23,6 +23,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 111° </span>
+          <span class="weather-forecast-temperature-min"> 99° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //formatting temperature and weather
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -93,3 +122,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 search("Las Vegas");
+displayForecast();
