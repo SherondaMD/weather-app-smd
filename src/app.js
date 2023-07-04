689fc007be7eb5afe8183b758febdce1ea923eea
SherondaMD <sheronda.mckee@gmail.com>
2023-07-03 19:04:22 -0700

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+//formatting date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 //formatting temperature and weather
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -11,7 +35,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let date = document.querySelector("#date");
-  date.innerHTML = showDate(response.data.time * 1000);
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
