646a414e8f7f387f5e4d384fe02663415742521e
SherondaMD <sheronda.mckee@gmail.com>
2023-07-03 18:24:53 -0700

    rewriting JS from scratch

--- src/app.js
+++ src/app.js
@@ -1,23 +1,9 @@
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("description");
-  let precipitationElement = document.querySelector("#precipitation");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-
-  temperatureElement.innnerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
-  precipitationElement.innerHTML = response.data.temperature.precipitation;
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
+
 let apiKey = "b8b61a4d34f03tcbf3192f94o59a0ba4";
-let city = "Phoenix";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Phoenix&key=${apiKey}&units=imperial`;
 
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Phoenix&key=b8b61a4d34f03tcbf3192f94o59a0ba4&units=imperial";
-
-axios.get(`${apiUrl}`).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
